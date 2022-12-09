ba29e65e450cac218e47b20ac3b54936b2718c4f
nhersal281 <nhersal281@g.educaand.es>
2022-12-09 22:11:51 +0100

    hacindo objetos

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -6,24 +6,41 @@ import 'package:provider/provider.dart';
 
 
 class DetailScreen extends StatelessWidget {
-  String competition;
-  DetailScreen({super.key, required this.competition});
+  final String competition;
+  const DetailScreen({super.key, required this.competition});
 
   @override
   Widget build(BuildContext context) {
-    final competitionProvider = Provider.of<FootballProvider>(context, listen: true); 
+    //final competitionProvider = Provider.of<FootballProvider>(context, listen: true); 
+
+    List<CompetitionsTable> listTableCompetition = [
+      CompetitionsTable(position: '1', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F5.png', name: 'Barcelona', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '2', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F26.png', name: 'Real Madrid', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '3', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F223.png', name: 'Real Sociedad', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '4', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F213.png', name: 'Athletic Club', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '5', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F3.png', name: 'Atlético Madrid', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '6', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F691.png', name: 'Real Betis', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '7', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F221.png', name: 'Osasuna', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '8', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F690.png', name: 'Rayo Vallecano', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '9', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F226.png', name: 'Villarreal', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '10', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F143.png', name: 'Valencia', points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '11', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F220.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '12', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F244.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '13', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F685.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '14', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F212.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '15', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F216.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '16', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F215.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '17', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F680.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '18', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F28.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '19', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F750.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+      CompetitionsTable(position: '20', squadLogo: 'https://image-service.onefootball.com/transform?w=21&h=21&dpr=2&image=https%253A%252F%252Fimages.onefootball.com%252Ficons%252Fteams%252F164%252F682.png', name: name, points: points, played: played, winned: winned, loosed: loosed, tie: tie, goalDifference: goalDifference),
+    ]; 
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tabla Clasificación'),
       ),
-      body: FutureBuilder(
-        future: competitionProvider.getCompetitionTable(competition),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) return const EmptyContainer();
-
-          List<CompetitionsTable> listTableCompetition = snapshot.data!;
-
-          return Container(
+      body: Container(
                 margin: const EdgeInsets.only(bottom: 30),
                 width: double.infinity,
                 child: ListView.builder(
@@ -32,10 +49,27 @@ class DetailScreen extends StatelessWidget {
                   itemBuilder: (BuildContext context, int index) =>
                       TableCompetitionItem(table: listTableCompetition[index]),
                 ),
-              );
+              ),
+      // FutureBuilder(
+      //   future: competitionProvider.getCompetitionTable(competition),
+      //   builder: (context, snapshot) {
+      //     if (!snapshot.hasData) return const EmptyContainer();
 
-        },
-      ),
+      //     List<CompetitionsTable> listTableCompetition = snapshot.data!;
+
+      //     return Container(
+      //           margin: const EdgeInsets.only(bottom: 30),
+      //           width: double.infinity,
+      //           child: ListView.builder(
+      //             itemCount: listTableCompetition.length,
+      //             scrollDirection: Axis.vertical,
+      //             itemBuilder: (BuildContext context, int index) =>
+      //                 TableCompetitionItem(table: listTableCompetition[index]),
+      //           ),
+      //         );
+
+      //   },
+      // ),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,10 +8,14 @@ import 'package:provider/provider.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
 
-    final competitionProvider = Provider.of<FootballProvider>(context, listen: true); 
+    //final competitionProvider = Provider.of<FootballProvider>(context, listen: true); 
+
+    final List<String> listCompetitions = ['1º División, La Liga Santander'];
 
     return Scaffold(
       appBar: AppBar(
@@ -25,27 +29,35 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
 
-      body: Center(
-        child: FutureBuilder(
-          future: competitionProvider.getListCompetitionsAvailable(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return const EmptyContainer();
+      body: ListView.builder(
+              itemCount: listCompetitions.length,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (BuildContext context, int index) =>
+                  CompetitionItem(competition: listCompetitions[index]),
+            ),
 
-            List<String> listCompetitions = snapshot.data!;
 
-            return Container(
-              margin: const EdgeInsets.only(bottom: 30),
-              width: double.infinity,
-              child: ListView.builder(
-                itemCount: listCompetitions.length,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (BuildContext context, int index) =>
-                    CompetitionItem(competition: listCompetitions[index]),
-              ),
-            );
-          },
-        )
-      ),
+      //Center(
+      //   child: FutureBuilder(
+      //     future: competitionProvider.getListCompetitionsAvailable(),
+      //     builder: (context, snapshot) {
+      //       if (!snapshot.hasData) return const EmptyContainer();
+
+      //       List<String> listCompetitions = snapshot.data!;
+
+      //       return Container(
+      //         margin: const EdgeInsets.only(bottom: 30),
+      //         width: double.infinity,
+      //         child: ListView.builder(
+      //           itemCount: listCompetitions.length,
+      //           scrollDirection: Axis.vertical,
+      //           itemBuilder: (BuildContext context, int index) =>
+      //               CompetitionItem(competition: listCompetitions[index]),
+      //         ),
+      //       );
+      //     },
+      //   )
+      // ),
     );
   }
 }
