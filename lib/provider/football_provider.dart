e0d186630a6b7218268fcba1cff5a7078e7f3cd1
nhersal281 <nhersal281@g.educaand.es>
2022-12-09 20:08:26 +0100

    Falta por poner el diseño más bonito en detail Scr

--- lib/provider/football_provider.dart
+++ lib/provider/football_provider.dart
@@ -18,7 +18,7 @@ class FootballProvider extends ChangeNotifier {
   Future<String> _getJsonData({String competition = '', String endPoint = '', String option = ''}) async {
     //Pass headers below 
     var jsonData = await http.get(Uri.parse('$_baseUrlCompetition$endPoint$competition$option'), headers: {
-      "X-RapidAPI-Key": "e4e96fe3bamsh0283c6c9ca2d165p10ccc4jsnc6eef5ecb535",
+      "X-RapidAPI-Key": "4c5f0244cdmsh03f622c619ab6c9p1b4e08jsnfbea51ce9229",
       "X-RapidAPI-Host": "football98.p.rapidapi.com"
       });
     return jsonData.body;
@@ -26,22 +26,24 @@ class FootballProvider extends ChangeNotifier {
 
   Future<List<String>> getListCompetitionsAvailable() async {
     
-    var data = await _getJsonData(endPoint: 'competitions');
+    if (listCompetitionsAvailable.isEmpty) {
+      var data = await _getJsonData(endPoint: 'competitions');
 
-    String dataClean = data.trim().substring(2, data.length-2);
-    listCompetitionsAvailable.clear();
-    for (var competition in dataClean.split(',')) {
-      listCompetitionsAvailable.add(competition);
+      String dataClean = data.trim().substring(2, data.length-2);
+      listCompetitionsAvailable.clear();
+      for (var competition in dataClean.split(',')) {
+        listCompetitionsAvailable.add(competition);
+      }
     }
 
     return listCompetitionsAvailable;
   }
 
     Future<List<CompetitionsTable>> getCompetitionTable(String competition) async {
-    var data = await _getJsonData(competition: competition, option: '/table');
+      var data = await _getJsonData(competition: competition, option: '/table');
 
       Iterable l = json.decode(data);
-      listCompetitionTable = List<CompetitionsTable>.from(l.map((model) => CompetitionsTable.fromMap(model)));
+      listCompetitionTable = List<CompetitionsTable>.from(l.map((model) => CompetitionsTable.fromMap(model)));        
 
     return listCompetitionTable;
   }
--- lib/widgets/competition_table_item.dart
+++ lib/widgets/competition_table_item.dart
@@ -12,18 +12,39 @@ class TableCompetitionItem extends StatelessWidget {
         
         Image.network(table.squadLogo, scale: 1.5,),
         const SizedBox(width: 15,),
+        if(table.position.length == 1)
+        Text('\t\t\t${table.position}º\t'),
+        if(table.position.length != 1)
         Text('\t\t${table.position}º'),
+        
         const SizedBox(width: 15,),
+        if(table.played.length == 1)
+        Text('\t\t${table.played}\t\t'),
+        if(table.played.length != 1)
         Text('\t\t${table.played}'),
+
         const SizedBox(width: 15,),
+        if(table.winned.length == 1) 
+        Text('\t\t\t${table.winned}\t\t'),
+        if(table.winned.length != 1)
         Text('\t\t${table.winned}'),
+        
         const SizedBox(width: 15,),
-        Text('\t\t${table.tie}'),
+        if(table.winned.length == 1)
+        Text('\t\t${table.winned}\t\t'),
+        if(table.winned.length != 1)
+        Text('\t\t${table.winned}'),
+
         const SizedBox(width: 15,),
+        if(table.loosed.length == 1)
+        Text('\t\t${table.loosed}\t\t'),
+        if(table.loosed.length != 1)
         Text('\t\t${table.loosed}'),
+
         const SizedBox(width: 15,),
-        Text('\t\t${table.goalDifference}'),
-        const SizedBox(width: 15,),
+        if(table.points.length == 1)
+        Text('\t\t${table.points}\t\t'),
+        if(table.points.length != 1)
         Text('\t\t${table.points}'),
         
       ],
